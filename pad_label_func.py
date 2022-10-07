bb42b000e563a85b4752e54ff41fcd08a8a5a1c6
TamuraLab <github@asr.info.gifu-u.ac.jp>
2022-10-07 19:33:48 +0900

    Add padding

--- pad_label_func.py
+++ pad_label_func.py
@@ -1,5 +1,6 @@
 import torch
 from phoneme_dataset import PAD_NUM, MASK_NUM
+from torch.nn import functional as F
 
 def pad_label(labels: torch.Tensor, outputs: torch.Tensor) -> torch.Tensor:
     """
@@ -14,4 +15,16 @@ def pad_label(labels: torch.Tensor, outputs: torch.Tensor) -> torch.Tensor:
         torch.Tensor: [BATCH_SIZE, Seq_length_B]
                       paddingされた後のlabelのTensor
     """
-    pass
+
+    batch_size = labels.size(0)
+    if labels.size(-1) < outputs.size(-2):
+        padding_length = outputs.size(-2) - labels.size(-1)
+        pad = torch.full(size=(batch_size, padding_length), fill_value=PAD_NUM)
+        labels = torch.cat([labels, pad], dim=1)
+    return labels
+
+if __name__ == "__main__":
+    PAD_NUM = 2
+    labels = torch.tensor([[1, 2],[1, 2]])
+    outputs = torch.tensor([[[1, 0, 0], [0, 1, 0], [0, 1, 0]], [[0, 0, 1], [0, 0, 1],[1, 0, 0]]])
+    pad_label(labels, outputs)
--- pad_out_func.py
+++ pad_out_func.py
@@ -1,5 +1,6 @@
 import torch
 from phoneme_dataset import PAD_NUM, MASK_NUM
+from torch.nn.functional import one_hot
 
 def pad_out(labels: torch.Tensor, outputs: torch.Tensor) -> torch.Tensor:
     """
@@ -10,7 +11,7 @@ def pad_out(labels: torch.Tensor, outputs: torch.Tensor) -> torch.Tensor:
       ... Seq_length
       [0., 0., 0., 1., 0., ...]
     ],
-    ... BATCH_SIZE
+      ... BATCH_SIZE
      [[0., 0., 0., 1., 0., ...],
       [0., 0., 0., 1., 0., ...],
       ... Seq_length
@@ -26,4 +27,27 @@ def pad_out(labels: torch.Tensor, outputs: torch.Tensor) -> torch.Tensor:
         torch.Tensor: [BATCH_SIZE, Seq_length_A, Vocab_size]
                       paddingされた後のoutputsのTensor
     """
-    pass
+
+    labels_length = labels.shape[1]
+    outputs_length = outputs.shape[1]
+
+    batch_size = labels.size(0)
+    #print(batch_size)
+    if labels_length > outputs_length:
+      padding_length = labels_length - outputs_length
+      #print(padding_length)
+      pad = torch.full(size=(batch_size, padding_length), fill_value=PAD_NUM)
+      pad = one_hot(pad)
+      outputs = torch.cat([outputs,pad],dim = 1)
+      outputs_length = outputs.shape[1]
+      #print(labels.shape,outputs.shape)
+      #print(labels)
+      #print(outputs)
+      assert labels_length == outputs_length, "labelとoutputの長さが違うよ"
+    return outputs
+
+if __name__ == "__main__":
+    PAD_NUM = 2
+    labels = torch.tensor([[1, 2, 0, 1, 2],[1, 2, 0, 1, 2]])
+    outputs = torch.tensor([[[1, 0, 0], [0, 1, 0], [0, 1, 0]], [[0, 0, 1], [0, 0, 1],[1, 0, 0]]])
+    pad_out(labels, outputs)
